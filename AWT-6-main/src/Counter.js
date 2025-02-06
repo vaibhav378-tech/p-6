101b7f0fa95b32f76bf24f1a1f9243a497463f58
vaibhav378-tech <vaibhavpatil.it22@scet.ac.in>
2025-02-06 10:25:04 +0530

    final commit

---
+++ AWT-6-main/src/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import './Counter.css'
+
+function Counter() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
